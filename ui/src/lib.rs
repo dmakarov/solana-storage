c9b7bd4b334c871734d44366eb510a1708909769
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2023-11-03 12:16:33 -0400

    Refactor UI data management into library

--- ui/src/lib.rs
+++ ui/src/lib.rs
@@ -1,3 +1,19 @@
+#![allow(non_snake_case)]
+
+use dioxus::prelude::*;
+
+pub fn Client(cx: Scope) -> Element {
+    render! {"Client application"}
+}
+
+pub fn Validator(cx: Scope) -> Element {
+    render! {"Validator log"}
+}
+
+pub fn Commands(cx: Scope) -> Element {
+    render! {"Commands"}
+}
+
 pub fn compile_smart_contract() {
     println!("RUST_LOG=info cargo build-sbf --manifest-path=program/Cargo.toml");
 }
@@ -7,9 +23,9 @@ pub fn deploy_smart_contract() {
 }
 
 pub fn run_client_with_options() {
-    println!("cargo run --manifest-path=client/Cargo.toml -- -C config.yml -k program/target/deploy/storage-keypair.json -u localhost create 255 255 0");
+    println!("cargo r -p client -- -C config.yml -k program/target/deploy/storage-keypair.json -u localhost create 255 255 0");
 }
 
 pub fn run_test_validator() {
-    println!("solana-test-validator -C ~/work/try/simple-solana-program/config.yml -l ~/work/try/simple-solana-program/test-ledger");
+    println!("solana-test-validator -C config.yml -l test-ledger");
 }
